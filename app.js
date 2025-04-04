37dbec5404f7b3e2e1be7b2b4b6e746a33c0d7c9
Pugalendhi_M <Pugalendhi26jan@gmail.com>
2025-04-04 18:21:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = 80;
 
-app.get('/hello', (req, res) => {
-  res.send('Hello, DevOps World!');
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js ECS!');
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
