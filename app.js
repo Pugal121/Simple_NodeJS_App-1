e886a4836c4b994de81c2a7700d2a1f6cca00565
Pugalendhi_M <Pugalendhi26jan@gmail.com>
2025-04-05 15:09:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 80;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Pugal, are u there!');
+  res.send('Hello from Pugal, Are u there!');
 });
 
 app.listen(PORT, () => {
